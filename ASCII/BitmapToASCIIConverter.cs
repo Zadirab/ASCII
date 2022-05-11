ecdbc24cd211144ad7272f8d1abb46bd0a259e21
user <user@WIN-MID5R4MIGJQ>
2022-05-11 17:13:08 +0300

    Добавьте файлы проекта.

---
+++ ASCII/BitmapToASCIIConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII
+{
+    class BitmapToASCIIConverter
+    {
+        private readonly char[] _asciiTable = { '.', ',', ':', '+', '*', '?', '%', 'S', '#', '@' };
+        private readonly Bitmap _bitmap;
+        public BitmapToASCIIConverter(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+        }
+
+        public char[][] Convert()
+        {
+            var image = new char[_bitmap.Height][];
+
+            for (int y = 0; y < _bitmap.Height; y++)
+            {
+                image[y] = new char[_bitmap.Width];
+                for (int x = 0; x < _bitmap.Width; x++)
+                {
+                    int MapIndex = (int)Map(_bitmap.GetPixel(x, y).R, 0, 255, 0, _asciiTable.Length - 1);
+                    image[y][x] = _asciiTable[MapIndex];
+                }
+            }
+
+            return image;
+        }
+        private float Map(float valueToMap, float start1, float stop1, float start2, float stop2)
+        {
+            return ((valueToMap - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+    }
+}
---
+++ ASCII/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ASCII
+{
+    class Program
+    {
+        private const double WIDTH_OFFSET = 1.5;
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image | *.bmp; *.png; *.jpg; *JPEG;"
+            };
+
+            Console.WriteLine("Press enter to start...\n");
+
+            while (true)
+            {
+                Console.ReadLine();
+
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                {
+                    continue;
+                }
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGrayscale();
+
+                var converter = new BitmapToASCIIConverter(bitmap);
+                var rows = converter.Convert();
+                foreach (var row in rows)
+                {
+                    Console.WriteLine(row);
+                }
+                Console.SetCursorPosition(0, 0);
+            }
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var MaxWidth = 600;
+            var NewHeight = bitmap.Height / WIDTH_OFFSET * MaxWidth / bitmap.Width;
+            if (bitmap.Width > MaxWidth || bitmap.Height > NewHeight)
+            {
+                bitmap = new Bitmap(bitmap, new Size(MaxWidth, (int)NewHeight));
+            }
+            return bitmap;
+        }
+    }
+}
